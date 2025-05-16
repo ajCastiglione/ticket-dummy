c4797d87d94f4110480b44717a66d23105dcd5a9
Antonio Castiglione <antonio@minervawebdevelopment.com>
2025-05-16 17:19:50 -0400

    refactored logging and handling redirect on ticket creation success

---
+++ src/actions/ticket.actions.ts
@@ -0,0 +1,63 @@
+// Backend file to handle actions for tickets.
+"use server";
+import { prisma } from "@/db/prisma";
+import { revalidatePath } from "next/cache";
+import { logEvent } from "@/utils/sentry";
+
+export async function createTicket(
+  prevState: { success: boolean; message: string },
+  formData: FormData
+): Promise<{ success: boolean; message: string }> {
+  try {
+    const subject = formData.get("subject") as string;
+    const description = formData.get("description") as string;
+    const priority = formData.get("priority") as string;
+
+    if (!subject || !description || !priority) {
+      logEvent(
+        "Validation Error: Missing ticket data fields",
+        "ticket",
+        { subject, description, priority },
+        "warning"
+      );
+      return {
+        success: false,
+        message: "All fields are required",
+      };
+    }
+
+    // Create new ticket.
+    const ticket = await prisma.ticket.create({
+      data: {
+        subject,
+        description,
+        priority,
+      },
+    });
+
+    logEvent(
+      `Ticket created successfully: ${ticket.id}`,
+      "ticket",
+      { ticketId: ticket.id },
+      "info"
+    );
+
+    revalidatePath("/tickets");
+
+    return { success: true, message: "Ticket created successfully" };
+  } catch (error) {
+    logEvent(
+      "Error occurred while creating ticket",
+      "ticket",
+      {
+        formData: Object.fromEntries(formData.entries()),
+      },
+      "error",
+      error
+    );
+    return {
+      success: false,
+      message: "An error occurred while trying to create the ticket",
+    };
+  }
+}
